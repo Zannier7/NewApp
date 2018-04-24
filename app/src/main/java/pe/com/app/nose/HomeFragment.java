413219479ff610bfdebf3262a253e20ab84c45e8
hansexe <hansexebio@upeu.edu.pe>
2018-04-24 14:28:07 -0500

    Perfil

--- app/src/main/java/pe/com/app/nose/HomeFragment.java
+++ app/src/main/java/pe/com/app/nose/HomeFragment.java
@@ -1,43 +1,35 @@
 package pe.com.app.nose;
 
 import android.content.Context;
+import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.bumptech.glide.Glide;
+import com.facebook.login.LoginManager;
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 
-public class HomeFragment extends Fragment {
-  @Nullable
+public class HomeFragment extends Fragment  {
+    @Nullable
 
-  private TextView uidTextView;
-    private TextView nameTextView;
-    private TextView emailTextView;
-    private ImageView photoImageView;
-    private TextView idTextView;
-    private GoogleApiClient googleApiClient;
-    private FirebaseAuth firebaseAuth;
-    private  FirebaseAuth.AuthStateListener firebaseAuthListener;
-
-  @Override
-  public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState){
-      final View view=inflater.inflate(R.layout.fragment_home,container,false);
-
-      uidTextView = (TextView) view.findViewById(R.id.uidTextView);
-      photoImageView = (ImageView) view.findViewById(R.id.photoImageView);
-      nameTextView = (TextView) view.findViewById(R.id.nameTextView);
-      emailTextView = (TextView) view.findViewById(R.id.emailTextView);
-      idTextView = (TextView) view.findViewById(R.id.idTextView);
-      return view;
-
-
-  }
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState){
+        return inflater.inflate(R.layout.fragment_home,container,false);
+    }
 }
